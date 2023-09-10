e47a49bc5fc12c7cd28c38196abc15e0d2a2d914
HOHOJO <ckwhs95@gmail.com>
2023-09-10 14:44:16 +0900

    공+패들 속도 조절, 아랫벽(임시) 만들기

--- Assets/Scripts/Controllers/BallController.cs
+++ Assets/Scripts/Controllers/BallController.cs
@@ -1,11 +1,17 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.U2D;
 
 public class BallController : MonoBehaviour
 {
-    [SerializeField] [RangeAttribute (1f, 1000f)] float speed = 1f;
+    [SerializeField][RangeAttribute(1f, 1000f)] float speed = 1f;
+    [SerializeField][RangeAttribute(0.5f, 5f)] float size = 1f;
+    bool starts = Input.GetMouseButton(0);
     public Rigidbody2D rd;
+
     float randomX, randomY;
 
     void Start()
@@ -14,9 +20,19 @@ public class BallController : MonoBehaviour
 
         randomX = 0f;
         randomY = 1f;
+        Starball();
+    }
 
+    void Update()
+    {
+        transform.localScale = new Vector3(size, size);
+        if (starts) { Starball(); }
+    }
+
+    public void Starball()
+    {
         Vector2 dir = new Vector2(randomX, randomY).normalized;
 
-        rd.AddForce (dir*speed);
+        rd.AddForce(dir * speed);
     }
 }
