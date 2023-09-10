ce9bbdbab29276e0a030db27c77107a32a2f8ec8
DoDokang318 <110431311+DoDokang318@users.noreply.github.com>
2023-09-10 11:49:28 +0900

    충돌처리 , StageManager코드리팩토링
    Brick생성시 brick count ++
    블럭이 공과 충돌하여 파괴시
    brick count --
    StageManager
    brick 생생시 stage1 오브잭트 자식객체로 생성 블럭생성 로직은 Stage1 매서드로 따로빼둠

--- Assets/Scripts/Managers/StageManager.cs
+++ Assets/Scripts/Managers/StageManager.cs
@@ -9,12 +9,36 @@ public class StageManager : MonoBehaviour
     private List<BrickData> brickDatas;
     [SerializeField]
     private GameObject brickPrefab;
-
-  
-
     public float xOffset = 2f;
+
+    [Header("Stage1")]
+    [SerializeField]
+    private GameObject stage1;
+
+    public int Stage1BrickCount;
+
+
+
+   
     void Start()
     {
+        Stage1();
+    }
+
+
+    public BrickController SpawnBrick(BrickType type)
+    {
+
+        var newBrick = Instantiate(brickPrefab).GetComponent<BrickController>();
+        newBrick.transform.SetParent(stage1.transform);
+        newBrick.brickData = brickDatas[(int)type];
+        newBrick.name = newBrick.brickData.BrickName;
+        return newBrick;
+    }
+
+    public void Stage1()
+    {
+
         int brickCount = brickDatas.Count;
         float currentX = -10f;
         float currentY = 0;
@@ -23,29 +47,20 @@ public class StageManager : MonoBehaviour
         {
             for (int j = 0; j < 9; j++)
             {
-               
+
                 currentX += xOffset;
                 brickPrefab.GetComponent<SpriteRenderer>().color = brickDatas[i].Color;
                 Vector3 newPosition = new Vector3(currentX, currentY, this.transform.position.z);
                 brickPrefab.transform.position = newPosition;
-              
+
                 var brick = SpawnBrick((BrickType)i);
+                Stage1BrickCount++;
                 brick.PrintBrick();
 
             }
-       
+
             currentY++;
             currentX = -10f;
         }
-    }
-
-    public BrickController SpawnBrick(BrickType type)
-    {
-       
-        var newBrick = Instantiate(brickPrefab).GetComponent<BrickController>();
-        newBrick.transform.SetParent(this.transform);
-        newBrick.brickData = brickDatas[(int)type];
-        newBrick.name = newBrick.brickData.BrickName;
-        return newBrick;
     }
 }
