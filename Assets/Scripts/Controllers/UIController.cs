b90a71ddbdc075dd355e5789e703b8f695e735b4
김명식 <scv940@gmail.com>
2023-09-11 17:11:22 +0900

    ###feautr : StartScene UI / 게임시작 /팝업 온오프 / 게임종료
    StartScene UI 기능 구현 완료

--- Assets/Scripts/Controllers/UIController.cs
+++ Assets/Scripts/Controllers/UIController.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
+    
     // Start is called before the first frame update
-    void Start()
+    public void GoMainScene()
     {
-        
+        SceneManager.LoadScene("MainScene");
+    }
+    public void GoStartScene()
+    {
+        SceneManager.LoadScene("StartScene");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnOffPopUP(GameObject gameObject)
     {
-        
+        if (gameObject.activeSelf == false)
+        {
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
     }
+
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+
 }
