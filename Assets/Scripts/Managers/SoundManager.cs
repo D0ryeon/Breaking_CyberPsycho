45d90703af92e2dfac22870e9b529a46163a7e28
IAK0401 <kym6812498@gmail.com>
2023-09-12 16:29:36 +0900

    ###featur SoundManager 추가
    사운드 추가 및 이펙트 진행중

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //private static SoundManager _instance;
+
+    public AudioSource BgmAudioSource;
+    public AudioSource EffectAudioSource;
+
+    public AudioClip BgmAudioClip;
+    public AudioClip effectAudioClip;
+
+    private bool isPlaying = false;
+
+    public void Start()
     {
-        
+        BgmAudioSource.clip = BgmAudioClip;
+        BgmAudioSource.Play();
+        //배열 관리 후 여러개의 음악 넣기
+    }
+
+    public void PlayEffectSound()
+    {
+        EffectAudioSource.clip = effectAudioClip;
+        //EffectAudioSource.
+        //배열관리하는법?
+    }
+
+    private void audioSourcePlay()
+    {
+        //audioSource.Play();
+        //BgmSound 멈추는법?
     }
 
     // Update is called once per frame
